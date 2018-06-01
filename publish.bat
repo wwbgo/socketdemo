b5c9d510ad24eade30cc3f4d722d9031a4132609
wangweibao <wangweibao@flexem.cn>
2018-06-01 17:19:15 +0800

    1

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
-docker build --rm -f Dockerfile/dockerfile -t 192.168.50.39/test/socketserver:v0.2 .
+docker build --rm -f dockerfile -t 192.168.50.39/test/socketserver:v0.2 .
 docker push 192.168.50.39/test/socketserver:v0.2
